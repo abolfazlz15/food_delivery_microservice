5efed2b984938c04b8c418251f0752d6b16f6787
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-04-28 17:34:22 +0330

    feat: add user model

---
+++ accounts/src/common/enum/user_role_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class UserRoleEnum(Enum):
+    USER = 0
+    ADMIN = 1
+    SUPER_ADMIN = 2
---
+++ accounts/src/model/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from src.common.enum.user_role_enum import UserRoleEnum
+from src.config.database import Base
+from src.model.base_model import BaseModel
+
+
+class User(BaseModel, Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    fullname: Mapped[str]
+    email: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+    )
+    password: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(
+        default=True,
+    )
+    role: Mapped[UserRoleEnum] = mapped_column(
+        default=UserRoleEnum.USER,
+    )
