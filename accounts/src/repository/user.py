c4555a556f7b008fed4bf96fa8e62ce7f5cda2c4
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-04-29 13:30:43 +0330

    accounts-feat: add user repository

---
+++ accounts/src/repository/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+import sqlalchemy as sa
+from src.model.user import User
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_by_email(self, email: str) -> User | None:
+        """Fetch a user by email."""
+        if result := await self.session.scalar(sa.select(User).where(User.email == email)):
+            return result
+        return None
