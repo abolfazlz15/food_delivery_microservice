05ed9b7735266c57575d1a830409d0e5bdb10873
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-07-13 20:18:21 +0330

    accounts-feat: add auth token repository dependency function

--- accounts/src/depends.py
+++ accounts/src/depends.py
@@ -4,11 +4,21 @@ from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config.database import get_db
+from src.repository.auth_token import AuthTokenRepository
 from src.repository.user import UserRepository
-from src.repository_interface.user_repository_interface import UserRepositoryInterface
+from src.repository_interface.auth_token_repository_interface import \
+    AuthTokenRepositoryInterface
+from src.repository_interface.user_repository_interface import \
+    UserRepositoryInterface
 
 
 def get_user_repository(
     db_session: Annotated[AsyncSession, Depends(get_db)],
 ) -> UserRepositoryInterface:
     return UserRepository(db_session)
+
+
+def get_auth_token_repository(
+    db_session: Annotated[AsyncSession, Depends(get_db)],
+) -> AuthTokenRepositoryInterface:
+    return AuthTokenRepository(db_session)
