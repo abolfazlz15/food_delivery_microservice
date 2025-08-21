0e6e9c85950405ca08caf4b429343f708fd9d438
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2025-08-21 12:16:19 +0330

    accounts-refactor: move hash functions

--- accounts/src/service/user.py
+++ accounts/src/service/user.py
@@ -2,24 +2,22 @@ from src.common.exceptions.exceptions import (
     EntityNotFoundException,
     InvalidCredentialsException,
 )
-from src.common.security.password_hash import PasswordContext
+from src.common.security.password_hash import verify_password, hash_password
 from src.repository_interface.user_repository_interface import UserRepositoryInterface
 from src.schema.user import (
     ChangePasswordInSchema,
     UserFullDataSchema,
-    UserInDBSchema,
     UserReadSchema,
     UserUpdateSchema,
 )
-from src.service.auth import get_password_hash
 
 
 class UserService:
     def __init__(self, user_repository: UserRepositoryInterface) -> None:
         self.user_repository = user_repository
 
-    async def user_detail(self, current_user: UserInDBSchema):
-        return UserFullDataSchema(
+    async def user_detail(self, current_user: UserFullDataSchema):
+        return UserReadSchema(
             id=current_user.id,
             fullname=current_user.fullname,
             email=current_user.email,
@@ -31,15 +29,13 @@ class UserService:
 
     async def change_password(
         self,
-        user: UserInDBSchema,
+        user: UserFullDataSchema,
         password_data: ChangePasswordInSchema,
     ) -> UserReadSchema:
-        if not PasswordContext.verify_password(
-            password_data.current_password, user.password
-        ):
+        if not verify_password(password_data.current_password, user.password):
             raise InvalidCredentialsException(message="Password is wrong")
 
-        hashed_new_password = get_password_hash(password_data.new_password)
+        hashed_new_password = hash_password(password_data.new_password)
 
         if user_obj := await self.user_repository.update_user(
             user_id=user.id,
